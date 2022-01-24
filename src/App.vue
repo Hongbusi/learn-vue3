d09fae0bbc9ee87737f9b7e61c98b0e6cf9d42d6
Hongbusi <q951686662@qq.com>
2022-01-24 23:34:25 +0800

    chore: computed api

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div>
-    <input v-model="message">
-    <h2>{{ message }}</h2>
+    <h2>{{ firstName }} {{ lastName }}</h2>
+    <h2>{{ fullName }}</h2>
+    <button @click="changeName">修改</button>
   </div>
 </template>
 
 <script>
-import debounceRef from './hook/useDebounceRef';
+import { ref, computed } from 'vue';
 
 export default {
   setup() {
-    const message = debounceRef('Hello World', 100);
+    const firstName = ref('Kobe');
+    const lastName = ref('Bryant');
+
+    // 1. 传入一个 getter 函数
+    // const fullName = computed(() => `${firstName.value} ${lastName.value}`);
+
+    // 2. 传入一个对象，对象包含 getter/setter
+    const fullName = computed({
+      get: () => `${firstName.value} ${lastName.value}`,
+      set(newValue) {
+        const names = newValue.split(' ');
+        firstName.value = names[0];
+        lastName.value = names[1];
+      }
+    });
+
+
+    const changeName = () => {
+      fullName.value = 'h bs';
+    }
 
     return {
-      message
+      fullName,
+      changeName
     }
   }
 }

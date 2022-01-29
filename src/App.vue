4be8a293b6b57c7adba2435ee8e055d76e100e91
Hongbusi <q951686662@qq.com>
2022-01-29 22:48:12 +0800

    chore: useCounter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  Hello World!
+  <h1>Hello World!</h1>
+  <hr>
+  <h2>useCounter</h2>
+  <p>{{ counter }}</p>
+  <button @click="decrement">-1</button>
+  <button @click="increment">+1</button>
 </template>
 
 <script>
+import { useCounter } from './hooks/useCounter';
 import { useTitle } from './hooks/useTitle';
 
 export default {
   setup() {
+    const { counter, increment, decrement } = useCounter();
+
     const titleRef = useTitle();
 
     setTimeout(() => {
       titleRef.value = '测试 title';
     }, 3000);
+
+    return {
+      counter,
+      increment,
+      decrement
+    }
   }
 }
 </script>
---
+++ src/hooks/useCounter.js
@@ -0,0 +1,14 @@
+import { ref } from 'vue';
+
+export function useCounter() {
+  const counter = ref(0);
+
+  const increment = () => counter.value++;
+  const decrement = () => counter.value--;
+
+  return {
+    counter,
+    increment,
+    decrement
+  }
+}

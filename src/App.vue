8e4d45bb80de2ecac0a62170cee5948be7203700
Hongbusi <q951686662@qq.com>
2022-02-10 22:21:18 +0800

    feat: router.push

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,26 @@
   <div class="content">
     <router-link to="/home">Home</router-link>
     <router-link to="/about">About</router-link>
+    <button @click="jumpToAbout">关于</button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+import router from './router';
+
 export default {
   setup() {
+    const router = useRouter();
 
+    const jumpToAbout = () => {
+      router.push('/about')
+    }
+
+    return {
+      jumpToAbout
+    }
   }
 };
 </script>
